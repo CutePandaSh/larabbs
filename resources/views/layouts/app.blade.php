dfb0fb150f99348df7159ab4a1a90ddfd9cb5fc6
Yi Wang <denniswangyi@gmail.com>
2020-01-17 17:41:00 +0000

    topic content page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="@yield('description', 'Laravel ?????')">
 
   <!-- CSRF TOKEN -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
