cd4eeaa19f68c0ef7fb887cff74d96e00c0932e5
Yi Wang <denniswangyi@gmail.com>
2020-01-18 11:18:41 +0000

    repies data fill

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,6 +48,11 @@ class User extends Authenticatable implements MustVerifyEmailContract
         return $this->hasMany(Topic::class);
     }
 
+    public function repies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+
     public function isAuthorOf($model)
     {
         return $this->id == $model->user_id;
