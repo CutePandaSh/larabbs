1f6aa7156567575d0ac00250d2e66b16670babb1
Yi Wang <denniswangyi@gmail.com>
2020-01-22 10:55:45 +0000

    featrue enhance: add admin permission redirect

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
 
 class PagesController extends Controller
 {
@@ -10,5 +11,13 @@ class PagesController extends Controller
     public function root()
     {
         return view("pages.root");
+    }
+
+    public function permissionDenied()
+    {
+        if ( config('administrator.permission')() ) {
+            return redirect(url(config('administrator.uri')), 302);
+        }
+        return view("pages.permission_denied");
     }
 }
