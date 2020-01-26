da94346d5d52a1f51891bc499b17f9df461d8b1e
Yi Wang <denniswangyi@gmail.com>
2020-01-26 22:28:29 +0000

    sub feature:add: upload avatar to S3

--- app/Handlers/imageUploadHandler.php
+++ app/Handlers/imageUploadHandler.php
@@ -5,6 +5,7 @@ namespace App\Handlers;
 use Illuminate\Support\Str;
 // use Image;
 use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\Storage;
 
 class ImageUploadHandler
 {
@@ -27,11 +28,18 @@ class ImageUploadHandler
         $file->move($upload_path, $file_name);
 
         if ($max_width && $extension != 'gif') {
-            $this->reduceSize($upload_path . '/' . $file_name, $max_width);
+            $this->reduceSize("$upload_path/$file_name", $max_width);
+       }
+
+        if (env('UPLOAD_DISK') == 's3'){
+            Storage::disk('s3')->putFileAs($folder_name, "$upload_path/$file_name", $file_name);
+            $file_path = Storage::disk('s3')->url("$folder_name/$file_name");
+        } else {
+            $file_path = config('app.url') . "/$folder_name/$file_name";
         }
 
         return [
-            'path' => config('app.url') . "/$folder_name/$file_name",
+            'path' => $file_path,
         ];
     }
 
