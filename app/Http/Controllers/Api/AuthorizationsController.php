cb217fbe1ea370a7ed43ba25ed6445d5e9532631
Yi Wang <denniswangyi@gmail.com>
2020-01-28 17:23:51 +0000

    sub-feature:add:Api Message Localization

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -69,7 +69,7 @@ class AuthorizationsController extends Controller
         $credentials['password'] = $request->password;
 
         if (!$token=\Auth::guard('api')->attempt($credentials)) {
-            throw new AuthenticationException('用户名密码错误！');
+            throw new AuthenticationException(trans('auth.failed'));
         }
 
         return $this->responseWithToken($token)->setStatusCode(201);
